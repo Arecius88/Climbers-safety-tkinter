7282ddbdb7e0c4023e6eec035d13127b2074f8ae
arecius88 <arecius88@gmail.com>
2024-06-04 11:37:02 +0200

    User input validation

--- climber-tk.py
+++ climber-tk.py
@@ -1,28 +1,50 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.messagebox import showwarning, showinfo
 
 # Configiration
 root_window_title = "Climbers safety"
 window_size = "250x150"
-sandbag_weight = 15 
+sandbag_weight = 15 # in kg
 
+def validate_user_input(user_input):
+    try:
+        value = int(user_input)
+        if _validate_above_zero(value):
+            return value
+    except ValueError:
+        showwarning(title="Error", message="Weight must be a number")
 
+def _validate_above_zero(user_input):
+    if user_input > 0:
+        return True
+    else: 
+        showwarning(title="Error", message="Weight must be over 0")
+
+    
 def submit_button():
     global amount_sandbags 
     
     #Get value
     climber_weight = climber_var.get()
     belayer_weight = belayer_var.get()
+    climber_weight = validate_user_input(climber_weight)
+    belayer_weight = validate_user_input(belayer_weight)
     
-    # Calculations 
-    weight_diff = climber_weight / belayer_weight
     
-    if weight_diff > 1.2:
-        amount_sandbags = round((climber_weight - belayer_weight) / sandbag_weight)
-        output_field.config(text=f"You need {amount_sandbags} sandbags")
+    if climber_weight and belayer_weight:
+        weight_diff = climber_weight / belayer_weight
+
+        if weight_diff > 1.2:
+            amount_sandbags = round((climber_weight - belayer_weight) / sandbag_weight)
+            output_field.config(text=f"You need {amount_sandbags} sandbags")
+        
+        else: 
+            showinfo(title="Number of sandbags", message="You don't need sandbags")
     
-    else: 
-        output_field.config(text=f"You don't need sandbags")
+
+
+
 
 
 # Create a Window
@@ -31,8 +53,8 @@ root.title(root_window_title)
 root.geometry(window_size)
 
 # Set Entry variable
-climber_var = tk.IntVar()
-belayer_var = tk.IntVar()
+climber_var = tk.StringVar()
+belayer_var = tk.StringVar()
 
 # Set Entry default to empty string
 climber_var.set("")
