5e445ea1106f81b71091ad351a77828cafc2afd4
arecius88 <arecius88@gmail.com>
2024-06-04 10:33:01 +0200

    Commenting

--- climber-tk.py
+++ climber-tk.py
@@ -3,18 +3,19 @@ from tkinter import ttk
 
 # Configiration
 root_window_title = "Climbers saftey"
-window_size = "250x150".v
+window_size = "250x150"
 sandbag_weight = 15
 
 
 def submit_button():
+    global amount_sandbags 
+    
     #Get value
     climber_weight = climber_var.get()
     belayer_weight = belayer_var.get()
     
     # Calculations 
     weight_diff = climber_weight / belayer_weight
-    global amount_sandbags 
     
     if weight_diff > 1.2:
         amount_sandbags = round((climber_weight-belayer_weight)/sandbag_weight)
@@ -23,40 +24,43 @@ def submit_button():
     else: 
         output_field.config(text=f"You don't need sandbags")
 
-    
-    
 
 # Create a Window
 root = tk.Tk()
 root.title(root_window_title)
 root.geometry(window_size)
+
+# Set Entry variable
 climber_var = tk.IntVar()
 belayer_var = tk.IntVar()
 
-# Climber input
+# Set Entry default to empty string
+climber_var.set("")
+belayer_var.set("")
+
+# Heading label - Climber
 climber_label = ttk.Label(master=root, text="Insert Climbers Weight")
 climber_label.pack()
 
+# Create Entry field - Climber
 climber_input_field = ttk.Entry(master=root, textvariable=climber_var)
 climber_input_field.pack()
 
-# Belayer input
+# Heading label - Belayer
 belayer_label = ttk.Label(master=root, text="Insert Belayers Weight")
 belayer_label.pack()
 
+# Create Entry field - Belayer
 belayer_input_field = ttk.Entry(master=root, textvariable=belayer_var)
 belayer_input_field.pack()
 
-# Submit button
-submit_button = ttk.Button(master=root, text="Calculate Sandbags", command= submit_button)
-submit_button.pack()
+# Button for calculate number of sanbags belayer needs. 
+sandbag_button = ttk.Button(master=root, text="Calculate Sandbags", command= submit_button)
+sandbag_button.pack()
 
 # Output field
 output_field = ttk.Label(text=f"You will need this many sandbags", font=("Helvetica", 12))
 output_field.pack()
-
-
-
 
 
 # Run
